71dc3e0a3874ca027b585a4f9e4b4282fed37712
Tiffany24630 <sal24630@uvg.edu.gt>
2024-09-01 16:48:54 -0600

    Método constructor de la clase Objetos Extraviados terminado

--- Backend/ObjetoExtraviado.java
+++ Backend/ObjetoExtraviado.java
@@ -10,5 +10,16 @@ public class ObjetoExtraviado {
     private String estado;
     private boolean secretaria; 
 
+    public ObjetoExtraviado(String tipobjeto, String color, String size, String forma, String fecha, String location, String estado, boolean secretaria){
+        this.tipobjeto = tipobjeto;
+        this.color = color;
+        this.size = size;
+        this.forma = forma;
+        this.fecha = fecha;
+        this.location = location;
+        this.estado = estado;
+        this.secretaria = secretaria;
+    }
+
     
 }
