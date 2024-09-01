c8f8a2639cb45e09c1d3896ce452ddc8b2ceb984
Tiffany24630 <sal24630@uvg.edu.gt>
2024-09-01 11:32:02 -0600

    Atributos agregados en clase Objetos extraviados

--- Backend/ObjetoExtraviado.java
+++ Backend/ObjetoExtraviado.java
@@ -1,5 +1,14 @@
 package Backend;
 
 public class ObjetoExtraviado {
+    private String tipobjeto;
+    private String color;
+    private String size;
+    private String forma;
+    private String fecha;
+    private String location;
+    private String estado;
+    private boolean secretaria; 
 
+    
 }
