9fd8aade218c63519e5e199eb63dd739b2f8e804
Tiffany24630 <sal24630@uvg.edu.gt>
2024-09-19 23:13:49 -0600

    Comentarios agregados en la clase de Objetos extraviados

--- Backend/ObjetoExtraviado.java
+++ Backend/ObjetoExtraviado.java
@@ -1,6 +1,7 @@
 package Backend;
 
-public class ObjetoExtraviado {
+public class ObjetoExtraviado { //Clase de registro de objetos extraviado
+    //Atributos
     private String tipobjeto;
     private String color;
     private String size;
@@ -10,6 +11,7 @@ public class ObjetoExtraviado {
     private String estado;
     private boolean secretaria; 
 
+    //Constructor
     public ObjetoExtraviado(String tipobjeto, String color, String size, String forma, String fecha, String location, String estado, boolean secretaria){
         this.tipobjeto = tipobjeto;
         this.color = color;
@@ -21,66 +23,82 @@ public class ObjetoExtraviado {
         this.secretaria = secretaria;
     }
 
+    //Get de tipo de objeto
     public String getTipobjeto(){
         return tipobjeto;
     }
 
+    //Get de color
     public String getColor(){
         return color;
     }
 
+    //Get de tamaño
     public String getSize(){
         return size;
     }
 
+    //Get de forma
     public String getForma(){
         return forma;
     }
 
+    //Get de fecha
     public String getFecha(){
         return fecha;
     }
     
+    //Get de locación
     public String getLocation(){
         return location;
     }
 
+    //Get de estado
     public String getEstado(){
         return estado;
     }
 
+    //Get de secretaría
     public boolean getSecretaria(){
         return secretaria;
     }
 
+    //set de tipo de objeto
     public void setTipobjeto(String tipobjeto) {
         this.tipobjeto = tipobjeto;
     }
 
+    //Set de color
     public void setColor(String color) {
         this.color = color;
     }
-    
+
+    //Set de tamaño
     public void setSize(String size) {
         this.size = size;
     }
 
+    //Set de forma
     public void setForma(String forma) {
         this.forma = forma;
     }
 
+    //Set de fecha
     public void setFecha(String fecha) {
         this.fecha = fecha;
     }
 
+    //Set de locación
     public void setLocation(String location) {
         this.location = location;
     }
 
+    //Set de estado
     public void setEstado(String estado) {
         this.estado = estado;
     }
 
+    //Set de secretaría
     public void setSecretaria(boolean secretaria) {
         this.secretaria = secretaria;
     }
