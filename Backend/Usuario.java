56aa3f5afbfdf08e155a24b4699ef2f7c4161503
Hertzzz7 <huguiscarrillo@gmail.com>
2024-09-20 09:24:25 -0600

    Creación de los atributos de la clase usuario

--- Backend/Usuario.java
+++ Backend/Usuario.java
@@ -1,5 +1,12 @@
 package Backend;
 
 public class Usuario {
-
-}
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String contraseña;
+    private Date fechaNacimiento;
+    private String direccion;
+    private String telefono;
+ 
