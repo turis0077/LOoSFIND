e695c9d9a9e9dfb8397aee63e913d345d1db0d4d
Erick130705 <gar24402@uvg.edu.gt>
2024-09-19 20:16:00 -0600

    Update Usuario.java

--- Backend/Usuario.java
+++ Backend/Usuario.java
@@ -1,5 +1,39 @@
 package Backend;
 
 public class Usuario {
+        private String nombre;
+        private String correo;
+        private String contrasena;
+    
+        public Usuario(String nombre, String correo, String contrasena) {
+            this.nombre = nombre;
+            this.correo = correo;
+            this.contrasena = contrasena;
+        }
+    
+        public String getNombre() {
+            return nombre;
+        }
+    
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+    
+        public String getCorreo() {
+            return correo;
+        }
+    
+        public void setCorreo(String correo) {
+            this.correo = correo;
+        }
+    
+        public String getContrasena() {
+            return contrasena;
+        }
+    
+        public void setContrasena(String contrasena) {
+            this.contrasena = contrasena;
+        }
+    }
+    
 
-}
