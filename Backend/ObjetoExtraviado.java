263702af7c6f3ff3749555098574e4b4e6d0957b
Erick130705 <gar24402@uvg.edu.gt>
2024-09-19 20:14:09 -0600

    Update ObjetoExtraviado.java
    lo mismo

--- Backend/ObjetoExtraviado.java
+++ Backend/ObjetoExtraviado.java
@@ -2,4 +2,29 @@ package Backend;
 
 public class ObjetoExtraviado {
 
-}
+        private String descripcion;
+        private Usuario usuario;
+    
+        public ObjetoExtraviado(String descripcion, Usuario usuario) {
+            this.descripcion = descripcion;
+            this.usuario = usuario;
+        }
+    
+        public String getDescripcion() {
+            return descripcion;
+        }
+    
+        public void setDescripcion(String descripcion) {
+            this.descripcion = descripcion;
+        }
+    
+        public Usuario getUsuario() {
+            return usuario;
+        }
+    
+        public void setUsuario(Usuario usuario) {
+            this.usuario = usuario;
+        }
+    }
+    
+
