3a889643c99bb9f6380985aa6f56764c43885a37
Tiffany24630 <sal24630@uvg.edu.gt>
2024-09-19 23:17:26 -0600

    Carpeta Frontend agregada

---
+++ Frontend/VentanaLogin.java
@@ -0,0 +1,21 @@
+import java.net.PasswordAuthentication;
+import javax.swing.plaf.metal.MetalBorders.TextFieldBorder;
+
+public class VentanaLogin{ //Clase de ventana del login
+    //Atributos
+    private TextFieldBorder correo;
+    private PasswordAuthentication pasw;
+
+    //Método constructor
+    public VentanaLogin(TextFieldBorder correo, PasswordAuthentication pasw){
+        this.correo = correo;
+        this.pasw = pasw;
+    }    
+    
+    //Método handleLogin
+
+
+    //Método para validar credenciales
+
+
+}
